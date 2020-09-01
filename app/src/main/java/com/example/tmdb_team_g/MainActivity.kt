c9da2bc2fa734a9b95cbfb9bd06a4490cf1bcb09
Abdelhay Nader <abdelhaynader@yahoo.com>
2020-09-01 03:47:15 +0200

    merge ahmed khorkhash branch

--- app/src/main/java/com/example/tmdb_team_g/MainActivity.kt
+++ app/src/main/java/com/example/tmdb_team_g/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.tmdb_team_g
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import retrofit2.Call
+import android.telecom.Call
 import retrofit2.Callback
 import retrofit2.Response
 
@@ -12,17 +12,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var apiServices : ApiServices = RetrofitClient.getClient().create(ApiServices::class.java)
-        var call : Call<MovieResponse> = apiServices.getMovieByApi("099a5418f2bec70523fe74e21f45b456")
+        var call : retrofit2.Call<MovieResponse> = apiServices.getMovieByApi("099a5418f2bec70523fe74e21f45b456")
 
 
         call.enqueue(object : Callback<MovieResponse> {
-            override fun onResponse(call: Call<MovieResponse>, response: Response<MovieResponse>) {
+            override fun onResponse(call: retrofit2.Call<MovieResponse>, response: Response<MovieResponse>) {
 
                 val movieResourseList : List<resultsList> = response.body()!!.results
 
             }
 
-            override fun onFailure(call: Call<MovieResponse>, t: Throwable) {
+            override fun onFailure(call: retrofit2.Call<MovieResponse>, t: Throwable) {
                 println(t.message)
                 call.cancel()
             }
