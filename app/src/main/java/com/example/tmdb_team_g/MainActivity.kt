bd3872c2c0c7eb668879fddd76437e38396ecd92
Ahmad Ossama <ahmad_otaca@hotmail.com>
2020-09-18 22:54:44 +0200

    Adding main_menu with items (Favourite, popular, Top rated) and bottom bar, linking them together and modifying MainActivity (All team members worked on this through zoom meeting)

--- app/src/main/java/com/example/tmdb_team_g/MainActivity.kt
+++ app/src/main/java/com/example/tmdb_team_g/MainActivity.kt
@@ -8,6 +8,8 @@ import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.lifecycle.Observer
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -21,12 +23,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navController = Navigation.findNavController(this,R.id.main_container_activityMain)
+        NavigationUI.setupWithNavController(main_bottom_bar,navController)
+
+
        /* mainViewModel.movieLiveData.observe(this,Observer{bindMovieData(it)})
 
         mainViewModel.onError.observe(this, Observer{handelMovieError(it)})
 
         mainViewModel.loadMovieData()
-    requestMovieData()*/
+       requestMovieData()*/
     }
 
 //    private fun requestMovieData() {
