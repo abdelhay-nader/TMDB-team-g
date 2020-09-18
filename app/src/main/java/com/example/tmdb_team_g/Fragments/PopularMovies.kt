fb935b6823017854aa3836a50ffbb67091d60c0a
Ahmad Ossama <ahmad_otaca@hotmail.com>
2020-09-18 20:44:35 +0200

    Added three blank fragments and dependencies of navigation components (All team members worked on this through zoom meeting)

---
+++ app/src/main/java/com/example/tmdb_team_g/Fragments/PopularMovies.kt
@@ -0,0 +1,22 @@
+package com.example.tmdb_team_g.Fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.tmdb_team_g.R
+
+class PopularMovies : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_popular_movies, container, false)
+    }
+
+
+}
