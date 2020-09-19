3e4b6dc5a3724c8082e2fd26adbfed2890390c8c
Ahmad Ossama <ahmad_otaca@hotmail.com>
2020-09-19 22:11:43 +0200

    TopRated Fragment is pointing to the MainViewModel of the activity and adding observation on TopMovie Live Data

--- app/src/main/java/com/example/tmdb_team_g/Fragments/TopRated.kt
+++ app/src/main/java/com/example/tmdb_team_g/Fragments/TopRated.kt
@@ -5,9 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.observe
+import com.example.tmdb_team_g.MainViewModel
 import com.example.tmdb_team_g.R
 
 class TopRated : Fragment() {
+
+
+    private lateinit var mainViewModel : MainViewModel
 
 
 
@@ -15,8 +22,28 @@ class TopRated : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        mainViewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
+
+        mainViewModel.topMovieLiveData.observe(viewLifecycleOwner,{
+
+        })
+
+        mainViewModel.onError.observe(viewLifecycleOwner,{
+            Toast.makeText(context,it, Toast.LENGTH_LONG).show()
+        })
+
+        mainViewModel.loadTopMovieData()
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_top_rated, container, false)
+
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+
 
 }
