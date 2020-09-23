9d030f17753cfd19f11f0f6a2ac6d853be9df45f
Abdelhay Nader <abdelhaynader@yahoo.com>
2020-09-23 20:42:17 +0200

    create entity for Top Movies List for sceond Api ( all team members worked on this through zoom meeting )

--- app/src/main/java/com/example/tmdb_team_g/TopMovieResponse.kt
+++ app/src/main/java/com/example/tmdb_team_g/TopMovieResponse.kt
@@ -1,13 +1,18 @@
 package com.example.tmdb_team_g
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
 data class TopMovieResponse (
     val results : List<resultsList2>
 )
 
-
+@Entity(tableName = "movie_table_2")
 data class resultsList2 (
 
+    @PrimaryKey
     val id : Long,
+
     val original_title : String,
     val poster_path : String,
     val original_language : String,
